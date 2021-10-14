b34b32db3dd6651040ce16f84069de5501337f9a
IidaPeltonen <n0peii00@students.oamk.fi>
2021-10-14 12:16:46 +0300

    pohjat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react';
+import axios from 'axios';
+
+const URL= 'http://localhost/ostosphp/index.php';
 
 function App() {
+  /* const [tasks, setTasks] = useState([]);
+  
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setTasks(response.data)
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      })
+  }, [])
+ */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ol>
+        laalaa
+      </ol>
     </div>
   );
 }
