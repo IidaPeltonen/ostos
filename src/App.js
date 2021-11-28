dc33d07858e1be10e2b0750ec4e822bb31388e4d
IidaPeltonen <n0peii00@students.oamk.fi>
2021-11-28 17:21:20 +0200

    typo

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
     const json = JSON.stringify({description:item, amount:amount})
     axios.post(URL + 'add.php',json,{
       headers: {
-        'Content-Type' : 'applicationJ/json'
+        'Content-Type' : 'application/json'
       }
     })
     .then((response) => {
@@ -49,63 +49,6 @@ function App() {
       alert(error.response ? error.response.data.error : error);
     });
   }
-/* 
-  function setEditedTask(task) {
-    setEditTask(task);
-    setEditDescription(task?.description);
-  }
-
-  function update(e) {
-    e.preventDefault();
-    const json= JSON.stringify({id:editTask.id, description:editDescription})
-    axios.post(URL + 'update.php', json, {
-      headers: {
-        'Content-Type' : 'application/json'
-      }
-    })
-    .then((response) => {
-      tasks[(tasks.findIndex(task => task.id === editTask.id))].description = editDescription;
-      setTasks([...tasks]);
-      setEditedTask(null);
-    }).catch (error => {
-      alert(error.response ? error.response.data.error : error);
-    });
-  } 
-
- return (
-    <div className="container">
-      <h3>Todo list</h3>
-      <form onSubmit={save}>
-        <label>New task</label>
-        <input value={task} onChange={e => setTask(e.target.value)} />
-        <button>Save</button>
-      </form>
-      <ol>
-        {tasks?.map(task => (
-          <li key={task.id}>
-            {editTask?.id !== task.id &&
-            task.description
-            }
-            {editTask?.id === task.id &&
-            <form onSubmit={update}>
-              <input value={editDescription} onChange={e => setEditDescription(e.target.value)}/>
-              <button>Save</button>
-              <button type="button" onClick={() => setEditedTask(null)}>Cancel</button>
-              </form>
-            }
-            <a className="delete" onClick={() => remove(task.id)} href="#">
-              Delete
-            </a>&nbsp;
-            {editTask === null &&
-              <a className="edit" onClick={() => setEditedTask(task)} href="#">
-                Edit
-              </a>
-            }
-          </li>
-        ))}
-      </ol>
-    </div>
-  ); */
 
   return (
     <div className="container">
@@ -124,19 +67,6 @@ function App() {
           </tr>
         ))}
       </table>
-
-
-
-      {/* <ul class="a">
-        {items?.map(item => (
-          <li key={item.id} >&nbsp;    
-          {item.description}&nbsp;{item.amount}&nbsp;
-          <a href="#" className="delete" onClick={() => remove(item.id)}>
-            Delete
-          </a>
-          </li>
-        ))}
-      </ul> */}
     </div>
     
   )
